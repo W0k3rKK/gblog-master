8fc402a9e10a8b1ab16c4055d409473ccbae1ffb
W0k3rKK <570488980@qq.com>
2023-03-26 18:06:26 +0800

    后端逻辑完成，前端搭建

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -10,3 +13,7 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(Element)
+
+Vue.prototype.$axios = axios //
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,65 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
+import BlogDetail from '../views/BlogDetail.vue'
+import BlogEdit from '../views/BlogEdit.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/',
+        // name: 'home',
+        // component: HomeView
+        name: 'Index',
+        redirect: {name: 'Blog'}
+    },
+    {
+        // path: '/about',
+        // name: 'about',
+        path: '/login',
+        name: 'Login',
+        component: Login
+
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    },
+    {
+        path: '/blogs',
+        name: 'Blogs',
+        // 懒加载
+        component: () => import('../views/Blogs.vue')
+    },
+    {
+        path: '/blog/add', // 注意放在 path: '/blog/:blogId'之前
+        name: 'BlogAdd',
+        meta: {
+            requireAuth: true
+        },
+        component: BlogEdit
+    },
+    {
+        path: '/blog/:blogId',
+        name: 'BlogDetail',
+        component: BlogDetail
+    },
+    {
+        path: '/blog/:blogId/edit',
+        name: 'BlogEdit',
+        meta: {
+            requireAuth: true
+        },
+        component: BlogEdit
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
